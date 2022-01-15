d8f899935fa5e5c133af27aea841c9ce9fbd8e2e
Wasleny <waslenymp@gmail.com>
2022-01-15 13:23:28 -0300

    Show score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,14 +1,51 @@
 import { BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
 import "./styles.css";
 
-function MovieStars() {
+type Props = {
+  score: number;
+};
+
+type StarProps = {
+  fill: number;
+};
+
+function getFills(score: number) {
+  const fills = [0, 0, 0, 0, 0];
+
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+function Star({fill}:StarProps) {
+      if (fill === 0) {
+        return <BsStar color="#ffba01" />
+      } else if(fill === 1) {
+        return <BsStarFill color="#ffba01" />
+      } else {
+        return <BsStarHalf color="#ffba01" />
+      }
+}
+
+function MovieStars({ score }: Props) {
+  const fills = getFills(score);
+
   return (
     <div className="dsmovie-stars-container">
-      <BsStarFill color="#ffba01" />
-      <BsStarFill color="#ffba01" />
-      <BsStarFill color="#ffba01" />
-      <BsStarHalf color="#ffba01" />
-      <BsStar color="#ffba01" />
+      <Star fill={fills[0]} />
+      <Star fill={fills[1]} />
+      <Star fill={fills[2]} />
+      <Star fill={fills[3]} />
+      <Star fill={fills[4]} />
     </div>
   );
 }
